b001b364457965281b421aeeedf04a5786abe7cf
jaymarc21 <>
2025-03-08 01:15:43 +0800

    Last Sync: 2025-03-07 17:15:43 (Mobile)

--- problem_01.py
+++ problem_01.py
@@ -8,3 +8,11 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+print("Simple Calculator Program")
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+print(f"The sum is {num1 + num2}")
+print(f"The difference is {num1 - num2}")
+print(f"The product is {num1 * num2}")
+print(f"The quotient is {(num1 / num2):.2f}")
