703b7df0891769965651421a506e18121b7cdeae
jaymarc21 <>
2025-03-08 01:18:36 +0800

    Last Sync: 2025-03-07 17:18:36 (Mobile)

--- problem_02.py
+++ problem_02.py
@@ -1,4 +1,4 @@
-# YOUR NAME
+# Jay Marc Custodio 
 # ITELEC2
 # Problem Set 01 - Problem 02
 # Square the Number Program
@@ -8,3 +8,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+print("Square the Number Program")
+number = int(input("Enter a number: "))
+print(f"The square of {number} is {(number * number):.2f}")
+
